c9333c99156f9707bcaadde079ab5d9aa4ce4408
ozel981 <ozel981@interia.pl>
2020-12-01 18:10:19 +0100

    Octree tree

---
+++ Colors reduction/Colors reduction/OctreeTree/Octree.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Colors_reduction.OctreeTree
+{
+    class Octree
+    {
+        private IOctreeNode root;
+        public int ColorsCount { get; private set; }
+
+        public Octree()
+        {
+            root = new OctreeNode();
+            ColorsCount = 0;
+        }
+
+        public void AddColor(Color color)
+        {
+            ColorsCount += root.AddColor(color, 0);
+        }
+
+        public Color GetColor(Color color)
+        {
+            return root.GetColor(color, 0);
+        }
+
+        public void ReduceColorsCount(int limit)
+        {
+            while (ColorsCount > limit)
+            {
+                int CountOfReducedColors = 0;
+                IOctreeNode ReducedNode;
+                IOctreeNode ReducedNodeParent;
+                (ReducedNodeParent, ReducedNode) = GetReducedNode(root);
+                foreach (IOctreeNode node in ReducedNode.GetNodes())
+                {
+                    if (node != null) CountOfReducedColors++;
+                }
+                if (ReducedNodeParent == null)
+                {
+                    root = new OctreeLeaf(ReducedNode.GetAvgColor(), ReducedNode.GetColorCount());
+                }
+                else for (int i = 0; i < 8; i++)
+                {
+                    if (ReducedNodeParent.GetNodes()[i] == ReducedNode)
+                    {
+                        ReducedNodeParent.GetNodes()[i] = new OctreeLeaf(ReducedNode.GetAvgColor(), ReducedNode.GetColorCount());
+                    }
+                }
+                ColorsCount -= CountOfReducedColors;
+                ColorsCount++;
+            }
+        }
+
+        private (IOctreeNode parent, IOctreeNode node) GetReducedNode(IOctreeNode root)
+        {
+            int maxIndex = 0;
+            int minColorCount = int.MaxValue;
+            IOctreeNode parent = null;
+            IOctreeNode node = null;
+            DFS(root, null, null, ref node, ref parent, 0, ref maxIndex, ref minColorCount);
+            return (parent, node);
+        }
+
+        private void DFS(IOctreeNode node, IOctreeNode parent, IOctreeNode parentParent,
+                         ref IOctreeNode rNode, ref IOctreeNode rParentNode,
+                         int index, ref int maxIndex, ref int minColorCount
+                        )
+        {
+            if (node == null) return;
+            if (node.GetNodes() == null)
+            {
+                if (index > maxIndex || (index == maxIndex && parent.GetColorCount() < minColorCount))
+                {
+                    rNode = parent;
+                    rParentNode = parentParent;
+                    maxIndex = index;
+                    minColorCount = parent.GetColorCount();
+                }
+                return;
+            }
+            foreach (IOctreeNode n in node.GetNodes())
+            {
+                DFS(n, node, parent, ref rNode, ref rParentNode, index + 1, ref maxIndex, ref minColorCount);
+            }
+        }
+    }
+}
+
+
---
+++ Colors reduction/Colors reduction/OctreeTree/OctreeNode.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colors_reduction.OctreeTree
+{
+    interface IOctreeNode
+    {
+        IOctreeNode[] GetNodes();
+        int AddColor(Color color, int index);
+        Color GetColor(Color color, int index);
+        int GetColorCount();
+        Color GetAvgColor();
+    }
+
+    class OctreeNode : IOctreeNode
+    {
+        private IOctreeNode[] nodes;
+        public OctreeNode()
+        {
+            nodes = new IOctreeNode[8];
+        }
+        private int ColorImportance(int color, int index)
+        {
+            return ((color & (255 >> index)) >= (128 >> index)) ? 1 : 0;
+        }
+        public int AddColor(Color color, int index)
+        {
+            int nodeId = (ColorImportance(color.R, index) +
+                        2 * ColorImportance(color.G, index) +
+                        4 * ColorImportance(color.B, index));
+            if (nodes[nodeId] == null)
+            {
+                if(index == 7)
+                {
+                    nodes[nodeId] = new OctreeLeaf(color);
+                }
+                else
+                {
+                    nodes[nodeId] = new OctreeNode();
+                    nodes[nodeId].AddColor(color, index + 1);
+                }
+                return 1;
+            }
+            return nodes[nodeId].AddColor(color, index + 1);
+        }
+
+        public Color GetColor(Color color, int index)
+        {
+            int nodeId = (ColorImportance(color.R, index) +
+                        2 * ColorImportance(color.G, index) +
+                        4 * ColorImportance(color.B, index));
+            return nodes[nodeId].GetColor(color, index + 1);
+        }
+
+        public IOctreeNode[] GetNodes()
+        {
+            return nodes;
+        }
+
+        public int GetColorCount()
+        {
+            int colorCount = 0;
+            for(int i=0;i<8;i++)
+            {
+                if(nodes[i]!=null)
+                {
+                    colorCount += nodes[i].GetColorCount();
+                }
+            }
+            return colorCount;
+        }
+
+        public Color GetAvgColor()
+        {
+            int anvColorR = 0;
+            int anvColorG = 0;
+            int anvColorB = 0;
+            int n = 0;
+            foreach (IOctreeNode node in nodes)
+            {
+                if (node != null)
+                {
+                    Color avgColor = node.GetAvgColor();
+                    anvColorR += avgColor.R;
+                    anvColorG += avgColor.G;
+                    anvColorB += avgColor.B;
+                    n += node.GetColorCount();
+                }
+            }
+            if(n > 0)
+            {
+                return Color.FromArgb(anvColorR / n, anvColorG / n,anvColorB / n);
+            }
+            else
+            {
+                throw new Exception("Bad tree");
+            }
+        }
+    }
+
+    class OctreeLeaf : IOctreeNode
+    {
+        private Color color;
+        private int Count;
+
+        public OctreeLeaf(Color color, int count = 1)
+        {
+            this.color = color;
+            Count = 1;
+        }
+        public int AddColor(Color color, int index)
+        {
+            Count++;
+            return 0;
+        }
+
+        public Color GetAvgColor()
+        {
+            return color;
+        }
+
+        public Color GetColor(Color color, int index)
+        {
+            return color;
+        }
+
+        public int GetColorCount()
+        {
+            return Count;
+        }
+
+        public IOctreeNode[] GetNodes()
+        {
+            return null;
+        }
+    }
+}
