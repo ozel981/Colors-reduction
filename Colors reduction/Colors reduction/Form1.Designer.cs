91ffc5b4f7109b9534ca5c841c03de6522724992
ozel981 <ozel981@interia.pl>
2020-12-02 21:28:06 +0100

    Octree refactor - wrong reduction

--- Colors reduction/Colors reduction/Form1.Designer.cs
+++ Colors reduction/Colors reduction/Form1.Designer.cs
@@ -28,13 +28,292 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel5 = new System.Windows.Forms.TableLayoutPanel();
+            this.AlongStatus = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBoxAlong = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBoxAfter = new System.Windows.Forms.PictureBox();
+            this.AfterStatus = new System.Windows.Forms.Label();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel6 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel7 = new System.Windows.Forms.TableLayoutPanel();
+            this.Label = new System.Windows.Forms.Label();
+            this.ColorsLimit = new System.Windows.Forms.TrackBar();
+            this.ReduceColorsButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel5.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAlong)).BeginInit();
+            this.tableLayoutPanel4.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAfter)).BeginInit();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.tableLayoutPanel7.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ColorsLimit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 61.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 38.5F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel5, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel4, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(495, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(302, 444);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // tableLayoutPanel5
+            // 
+            this.tableLayoutPanel5.ColumnCount = 1;
+            this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel5.Controls.Add(this.AlongStatus, 0, 1);
+            this.tableLayoutPanel5.Controls.Add(this.groupBox2, 0, 0);
+            this.tableLayoutPanel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel5.Location = new System.Drawing.Point(3, 225);
+            this.tableLayoutPanel5.Name = "tableLayoutPanel5";
+            this.tableLayoutPanel5.RowCount = 2;
+            this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel5.Size = new System.Drawing.Size(296, 216);
+            this.tableLayoutPanel5.TabIndex = 1;
+            // 
+            // AlongStatus
+            // 
+            this.AlongStatus.AutoSize = true;
+            this.AlongStatus.Location = new System.Drawing.Point(3, 194);
+            this.AlongStatus.Name = "AlongStatus";
+            this.AlongStatus.Size = new System.Drawing.Size(38, 13);
+            this.AlongStatus.TabIndex = 2;
+            this.AlongStatus.Text = "Ready";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBoxAlong);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(3, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(290, 188);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Reduce along octree construction";
+            // 
+            // pictureBoxAlong
+            // 
+            this.pictureBoxAlong.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxAlong.Location = new System.Drawing.Point(3, 16);
+            this.pictureBoxAlong.Name = "pictureBoxAlong";
+            this.pictureBoxAlong.Size = new System.Drawing.Size(284, 169);
+            this.pictureBoxAlong.TabIndex = 0;
+            this.pictureBoxAlong.TabStop = false;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 1;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel4.Controls.Add(this.AfterStatus, 0, 1);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 2;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(296, 216);
+            this.tableLayoutPanel4.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBoxAfter);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(290, 188);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Reduce after octree construction";
+            // 
+            // pictureBoxAfter
+            // 
+            this.pictureBoxAfter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxAfter.Location = new System.Drawing.Point(3, 16);
+            this.pictureBoxAfter.Name = "pictureBoxAfter";
+            this.pictureBoxAfter.Size = new System.Drawing.Size(284, 169);
+            this.pictureBoxAfter.TabIndex = 0;
+            this.pictureBoxAfter.TabStop = false;
+            // 
+            // AfterStatus
+            // 
+            this.AfterStatus.AutoSize = true;
+            this.AfterStatus.Location = new System.Drawing.Point(3, 194);
+            this.AfterStatus.Name = "AfterStatus";
+            this.AfterStatus.Size = new System.Drawing.Size(38, 13);
+            this.AfterStatus.TabIndex = 1;
+            this.AfterStatus.Text = "Ready";
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.tableLayoutPanel6, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.tableLayoutPanel7, 0, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 61.5F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 38.5F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(486, 444);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // tableLayoutPanel6
+            // 
+            this.tableLayoutPanel6.ColumnCount = 3;
+            this.tableLayoutPanel6.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel6.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel6.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel6.Controls.Add(this.pictureBox, 1, 0);
+            this.tableLayoutPanel6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel6.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel6.Name = "tableLayoutPanel6";
+            this.tableLayoutPanel6.RowCount = 1;
+            this.tableLayoutPanel6.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel6.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel6.Size = new System.Drawing.Size(480, 267);
+            this.tableLayoutPanel6.TabIndex = 1;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(51, 3);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(378, 261);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // tableLayoutPanel7
+            // 
+            this.tableLayoutPanel7.ColumnCount = 2;
+            this.tableLayoutPanel7.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel7.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel7.Controls.Add(this.Label, 0, 0);
+            this.tableLayoutPanel7.Controls.Add(this.ColorsLimit, 1, 0);
+            this.tableLayoutPanel7.Controls.Add(this.ReduceColorsButton, 1, 1);
+            this.tableLayoutPanel7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel7.Location = new System.Drawing.Point(3, 276);
+            this.tableLayoutPanel7.Name = "tableLayoutPanel7";
+            this.tableLayoutPanel7.RowCount = 2;
+            this.tableLayoutPanel7.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel7.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel7.Size = new System.Drawing.Size(480, 165);
+            this.tableLayoutPanel7.TabIndex = 1;
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.Location = new System.Drawing.Point(3, 0);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(35, 13);
+            this.Label.TabIndex = 2;
+            this.Label.Text = "label1";
+            // 
+            // ColorsLimit
+            // 
+            this.ColorsLimit.Location = new System.Drawing.Point(243, 3);
+            this.ColorsLimit.Maximum = 1000;
+            this.ColorsLimit.Minimum = 1;
+            this.ColorsLimit.Name = "ColorsLimit";
+            this.ColorsLimit.Size = new System.Drawing.Size(234, 45);
+            this.ColorsLimit.TabIndex = 3;
+            this.ColorsLimit.Value = 1;
+            this.ColorsLimit.Scroll += new System.EventHandler(this.ColorsLimit_Scroll);
+            // 
+            // ReduceColorsButton
+            // 
+            this.ReduceColorsButton.Location = new System.Drawing.Point(243, 85);
+            this.ReduceColorsButton.Name = "ReduceColorsButton";
+            this.ReduceColorsButton.Size = new System.Drawing.Size(234, 34);
+            this.ReduceColorsButton.TabIndex = 4;
+            this.ReduceColorsButton.Text = "button1";
+            this.ReduceColorsButton.UseVisualStyleBackColor = true;
+            this.ReduceColorsButton.Click += new System.EventHandler(this.ReduceColorsButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Colors reduction";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel5.ResumeLayout(false);
+            this.tableLayoutPanel5.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAlong)).EndInit();
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.tableLayoutPanel4.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAfter)).EndInit();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.tableLayoutPanel7.ResumeLayout(false);
+            this.tableLayoutPanel7.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ColorsLimit)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.PictureBox pictureBoxAlong;
+        private System.Windows.Forms.PictureBox pictureBoxAfter;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel6;
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel7;
+        private System.Windows.Forms.TrackBar ColorsLimit;
+        private System.Windows.Forms.Button ReduceColorsButton;
+        private System.Windows.Forms.Label AlongStatus;
+        private System.Windows.Forms.Label AfterStatus;
     }
 }
 
--- Colors reduction/Colors reduction/Form1.cs
+++ Colors reduction/Colors reduction/Form1.cs
@@ -24,7 +24,7 @@ namespace Colors_reduction
             InitializeComponent();
             imagePath = Application.StartupPath;
             imagePath = Path.Combine(imagePath.Substring(0, imagePath.Length - 9), @"Images");
-            imagePath += "/lena.jpg";
+            imagePath += "/lena3.jpg";
             pictureBox.Image = new Bitmap(new Bitmap(imagePath), pictureBox.Width, pictureBox.Height);
             pictureBoxAfter.Image = new Bitmap(pictureBoxAfter.Width, pictureBoxAfter.Height);
             pictureBoxAlong.Image = new Bitmap(pictureBoxAlong.Width, pictureBoxAlong.Height);
@@ -36,18 +36,41 @@ namespace Colors_reduction
         {
             using (Graphics graphics = Graphics.FromImage(pictureBoxAfter.Image))
             {
-                //TODO: write drawing for reduced colors bitmap
+                graphics.DrawImage(PictureReduceAfterCreatingOctree.CalculateReducedColorsBitmap(ColorsLimit.Value, pictureBoxAfter.Size),new Point(0,0));
             }
+            pictureBoxAfter.Refresh();
+            AfterStatus.Text = "Ready";
         }
 
         private void DrawPictureWithReducedColorsAlongCreatigOctree()
-        {
+        { 
             using (Graphics graphics = Graphics.FromImage(pictureBoxAlong.Image))
             {
-                //TODO: write drawing for reduced colors bitmap
+                graphics.DrawImage(PictureReduceAlongCreatingOctree.CalculateReducedColorsBitmap(ColorsLimit.Value, pictureBoxAlong.Size), new Point(0, 0));
             }
+            pictureBoxAlong.Refresh();
+            AlongStatus.Text = "Ready";
         }
 
+        private void ColorsLimit_Scroll(object sender, EventArgs e)
+        {
+            ReduceColorsButton.Text = $"Reduce to {((TrackBar)sender).Value} colors";
+        }
 
+        private void ReduceColorsButton_Click(object sender, EventArgs e)
+        {
+            AfterStatus.Text = "Calculating...";
+            AlongStatus.Text = "Calculating...";
+            this.Refresh();
+            /*Task afterReduce = new Task(DrawPictureWithReducedColorsAfterCreatigOctree);
+            Task alongReduce = new Task(DrawPictureWithReducedColorsAlongCreatigOctree);
+            afterReduce.Start();
+            alongReduce.Start();*/
+            DrawPictureWithReducedColorsAlongCreatigOctree();
+            this.Refresh();
+            DrawPictureWithReducedColorsAfterCreatigOctree();
+            this.Refresh();
+
+        }
     }
 }
